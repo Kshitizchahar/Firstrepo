c5b0e7b9265ebbe50f35b7d550d3f18c5abf2209
kshitijmom <72298835+kshitijmom@users.noreply.github.com>
2020-10-16 20:33:55 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,1 +1,15 @@
 System.out.println("Kshitiz chahar");
+//program to add two numbers
+public class AddTwoIntegers {
+
+    public static void main(String[] args) {
+        
+        int first = 10;
+        int second = 20;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
